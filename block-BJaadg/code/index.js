150c353c0d4cfe750d9ce3c1893b92cd33efaaa8
ElisNiya <50265309+ElisNiya@users.noreply.github.com>
2021-11-03 22:00:35 +0100

    Update index.js

--- block-BJaadg/code/index.js
+++ block-BJaadg/code/index.js
@@ -7,15 +7,15 @@ let to = 'Arya Stark';
 /*
 1. Find the index of the first 'is' in the variable quote. And store it in a new variable named indexOfIs
 */
-
+indexOf('is')
 /*
 2. Find the character at the index indexOfIs (Problem 1) in quote.
 */
-
+include()
 /*
 3. Log the message saying `The index of first is in quote is 7`
 */
-
+charAt()
 /*
 4. Log the message for first 6 characters of quote like this.
   The character at index 0 is 'T'
@@ -25,12 +25,12 @@ let to = 'Arya Stark';
   The character at index 4 is 'e'
   The character at index 5 is ' '
 */
-
+charAt() is indexOf()
 /*
 5. Using the variable from , to and quote variable dispaly this message
   "Syrio Forel said There is only one thing we say to death: Not today to Arya Stark." (use concat method)
 */
-
+concat(from, quote, to)
 /*
 6. Does from, to and quote ends with "rk". Check all three.
 */
@@ -38,19 +38,19 @@ let to = 'Arya Stark';
 /*
 7. Does quote includes the word "Only"
 */
-
+includes("Only")
 /*
 8. Does quote includes the word " we"
 */
-
+includes("we")
 /*
 9. Find the index of the the word `we` in quote
 */
-
+indexOf("we")
 /*
 10. Split the quote into individual word and store it in a variable name quoteSplitted
 */
-
+let quoteSplitted = quote.split("")
 /*
 11. Change the word "today" in quoteSplitted to "tomorrow" and join all the words to form a sentance.
 */
